55756dd4900944d94ff6b8becbe9480a4f96c824
KirstenvdS <93930510+KirstenvdS@users.noreply.github.com>
2024-04-02 16:42:48 +0200

    Add files via upload

---
+++ exploratory_data_analysis.py
@@ -0,0 +1,13 @@
+# imports
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# load in data file
+data = pd.read_csv('ODI-2024.csv')
+
+# print(data.shape)
+# print(data.describe)
+
+
